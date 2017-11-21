ea197a5e8d4fb9203687d1eb98f1ee9359893213
Kashyap <skashyap@lowes.com>
2017-11-21 20:41:42 +0530

    Week 3 assignmnet

--- cachematrix.R
+++ cachematrix.R
@@ -3,12 +3,13 @@
 ## This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-    inv<-NULL 
-    set<- function(y){
-      x<<-y
-      inv<<-NULL
+    inv <- NULL 
+    set <- function(y){
+      x <<- y
+      inv <<- NULL
     }
-    get<-Function()x
+    get<-function()x
+    
     setinverse<-function(inverse) inv<<-inverse
     getinverse<-function()inv
     list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
@@ -25,6 +26,7 @@ cacheSolve <- function(x, ...) {
     message("getting cached data")
     return(inv)
   }
+  
   data <- x$get()
   inv <- solve(data, ...)
   x$setinverse(inv)
